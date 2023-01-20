b399aefa0a28a31bcffc76525934c995a8d5cfe0
Nicholas Kariniemi <nicholas.kariniemi@reaktor.com>
2023-01-20 11:06:19 +0200

    Add shebang for install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo cp files/gnome-xmonad.desktop /usr/share/xsessions/gnome-xmonad.desktop
 sudo cp files/gnome-xmonad /usr/libexec/gnome-xmonad
 sudo cp files/gnome-xmonad.session /usr/share/gnome-session/sessions/gnome-xmonad.session
