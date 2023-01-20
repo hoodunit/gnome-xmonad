e5b2f99276e9cbcde1081e5aca4876d337ec731b
Nicholas Kariniemi <nicholas.kariniemi@reaktor.com>
2023-01-20 10:26:58 +0200

    Add scripts and README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Xmonad + Gnome
+
+This is a script to install xmonad with Gnome session manager in Ubuntu since [gnome-session-xmonad](https://github.com/Gekkio/gnome-session-xmonad) is no longer supported.
+
+This is basically untested. It might work. Use at your own risk. It's up here for my own reference.
+
+Ubuntu version: 22.04 jammy
---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo cp files/gnome-xmonad.desktop /usr/share/xsessions/gnome-xmonad.desktop
+sudo cp files/gnome-xmonad /usr/libexec/gnome-xmonad
+sudo cp files/gnome-xmonad.session /usr/share/gnome-session/sessions/gnome-xmonad.session
+sudo cp files/xmonad-gnome.desktop /usr/share/applications/xmonad-gnome.desktop
+sudo cp files/xmonad-gnome /usr/bin/xmonad-gnome
