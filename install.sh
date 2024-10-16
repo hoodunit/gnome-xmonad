1c83613a9b8a304395f5248d9cf1f3c5998d8b65
Nicholas Kariniemi <nicholas.kariniemi@reaktor.com>
2024-10-16 09:07:54 +0300

    Set file permissions

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 sudo cp files/gnome-xmonad.desktop /usr/share/xsessions/gnome-xmonad.desktop
 sudo cp files/gnome-xmonad /usr/libexec/gnome-xmonad
+sudo chmod 755 /usr/libexec/gnome-xmonad
 sudo cp files/gnome-xmonad.session /usr/share/gnome-session/sessions/gnome-xmonad.session
 sudo cp files/xmonad-gnome.desktop /usr/share/applications/xmonad-gnome.desktop
 sudo cp files/xmonad-gnome /usr/bin/xmonad-gnome
-sudo mkdir /usr/lib/systemd/user/gnome-session@gnome-xmonad.target.d
+sudo chmod 755 /usr/bin/xmonad-gnome
+sudo mkdir -p /usr/lib/systemd/user/gnome-session@gnome-xmonad.target.d
 sudo cp files/session.conf /usr/lib/systemd/user/gnome-session@gnome-xmonad.target.d/session.conf
