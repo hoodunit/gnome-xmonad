685d1a35220ff35b17e07d36f079408035a8bd93
Nicholas Kariniemi <nicholas.kariniemi@reaktor.com>
2024-10-16 11:30:23 +0300

    Add note about compiling xmonad

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ sudo cp files/xmonad-gnome /usr/bin/xmonad-gnome
 sudo chmod 755 /usr/bin/xmonad-gnome
 sudo mkdir -p /usr/lib/systemd/user/gnome-session@gnome-xmonad.target.d
 sudo cp files/session.conf /usr/lib/systemd/user/gnome-session@gnome-xmonad.target.d/session.conf
+
+echo "Remember to create .xmonad/xmonad.hs and then run xmonad --recompile to make sure it compiles"
