ec6951e253d390a706ffd1dbd65662779c9e560d
Nicholas Kariniemi <nicholas.kariniemi@reaktor.com>
2024-10-16 08:57:12 +0300

    Make dir before moving file in

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ sudo cp files/gnome-xmonad /usr/libexec/gnome-xmonad
 sudo cp files/gnome-xmonad.session /usr/share/gnome-session/sessions/gnome-xmonad.session
 sudo cp files/xmonad-gnome.desktop /usr/share/applications/xmonad-gnome.desktop
 sudo cp files/xmonad-gnome /usr/bin/xmonad-gnome
+sudo mkdir /usr/lib/systemd/user/gnome-session@gnome-xmonad.target.d
 sudo cp files/session.conf /usr/lib/systemd/user/gnome-session@gnome-xmonad.target.d/session.conf
